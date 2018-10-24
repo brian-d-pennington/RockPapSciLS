f69ef641a0297fead4d7e20219341f0717bf5f4c
Brian Pennington <srsrec@gmail.com>
2018-10-24 14:47:19 -0500

    most classes created

---
+++ PlayerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class PlayerModel
+    {
+        public string playerName;
+        public int playerPoints;
+
+        public PlayerModel()
+        {
+            playerName = "no name yet";
+            playerPoints = 0;
+        }
+
+    }
+}
